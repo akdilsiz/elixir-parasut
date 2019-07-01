ada3c045c570475b71c29665e224c495b185b042
Abdulkadir DILSIZ <akdilsiz@tecpor.com>
2019-07-01 23:46:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Parasut client for elixir
+
+[![Hex.pm](https://img.shields.io/hexpm/v/parasut.svg)](https://hex.pm/packages/parasut)
+[![Hex.pm](https://img.shields.io/hexpm/dt/parasut.svg)](https://hex.pm/packages/parasut)
+
+***This project on going work***
+
+## Installation
+[available in Hex](https://hex.pm/packages/parasut), the package can be installed
+by adding `parasut` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:parasut, "~> 0.1.0"}
+  ]
+end
+```
+
+## Documentation
+[available HexDocs](https://hexdocs.pm/parasut).
+
+## Contribution
+
+All contributions are welcomed as long as you follow the conventions of *Elixir* language.
+
+## License
+
+Apache 2.0
---
+++ mix.exs
@@ -0,0 +1,58 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+defmodule Parasut.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :parasut,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      package: package(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      source_url: "https://github.com/akdilsiz/elixir-parasut"
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", ".formatter.exs", "LICENSE*"],
+      maintainers: ["Abdulkadir DILSIZ"],
+      licenses: ["Apache-2.0"],
+      description: "Parasut client for elixir",
+      links:  %{"GitHub" => "https://github.com/akdilsiz/elixir-parasut"}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:excoveralls, "~> 0.10", only: :test},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
